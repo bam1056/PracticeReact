160b5906b764a3fb7552dd96fd2927547a7f9561
Brett Macy <bam03584@aol.com>
2016-08-14 00:23:46 -0400

    Commented Picture.js

--- src/Picture.js
+++ src/Picture.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
 
+// Component only has an image, but the img has an onClick prop that takes the
+// reference to the function passed from App -> Page -> Picture via props and
+// causes 'home' to be passed as an argument to the App.js function which
+// re-renders the page based on the setState() which sets the currentScreen to
+// 'home'
 class Picture extends Component {
   navigateToHome = (e) => {
     e.preventDefault()
